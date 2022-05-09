e9e3d385e08d1b2e06443f8079c49e69bedaf8ba
code-zeen <77660676+code-zeen@users.noreply.github.com>
2022-05-09 15:14:39 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,37 @@
+// masonry
+function resizeGridItem(item){
+  grid = document.getElementsByClassName("grid")[0];
+  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+  item.style.gridRowEnd = "span "+rowSpan;
+}
+
+function resizeAllGridItems(){
+  allItems = document.getElementsByClassName("item");
+  for(x=0;x<allItems.length;x++){
+     resizeGridItem(allItems[x]);
+  }
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
+allItems = document.getElementsByClassName("item");
+for(x=0;x<allItems.length;x++){
+   imagesLoaded( allItems[x], resizeInstance);
+}
+
+function resizeInstance(instance){
+  item = instance.elements[0];
+  resizeGridItem(item);
+}
+
+// accordion menu jQuery
+$(".snb>li").mouseenter(function(){
+  $(this).children(".sub-menu").stop().slideDown()
+});
+$(".snb>li").mouseleave(function(){
+  $(this).children(".sub-menu").stop().slideUp()
+});
+
